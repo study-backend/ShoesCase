6e09f46fcfe4fa8d49cd772b23f1f99c2a8408ce
hankkuu <hankkuu@gmail.com>
2019-12-05 12:33:44 +0900

    tt

--- src/shop/shoes/controller/ReviewDeleteController.java
+++ src/shop/shoes/controller/ReviewDeleteController.java
@@ -1,26 +1,26 @@
-//
-//package shop.shoes.controller;
-//
-//import java.io.IOException;
-//import java.sql.SQLException;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import shop.shoes.service.ReviewService;
-//
-//public class ReviewDeleteController implements Controller {
-//
-//	@Override public ModelAndView handleRequest(HttpServletRequest request,
-//   HttpServletResponse response) throws SQLException, IOException {
-//   System.out.println("리뷰 delete 컨트롤러 호출"); int reviewId =0; //값 받아야할 듯? String
-//   reviewPwd = ""; //값 받아야 할듯? //ReviewService.delete(reviewId, reviewPwd);
+
+package shop.shoes.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import shop.shoes.service.ReviewService;
+
+public class ReviewDeleteController implements Controller {
+
+	@Override public ModelAndView handleRequest(HttpServletRequest request,
+   HttpServletResponse response) throws SQLException, IOException {
+   System.out.println("리뷰 delete 컨트롤러 호출"); int reviewId =0; //값 받아야할 듯? String
+   reviewPwd = ""; //값 받아야 할듯? //ReviewService.delete(reviewId, reviewPwd);
+   
+   //이미 db에 날라가고 데이터가 알아서 처리될 것임 //때문에 ajax로 통신해야 새로고침없이 화면 갱신될거같음
 //   
-//   //이미 db에 날라가고 데이터가 알아서 처리될 것임 //때문에 ajax로 통신해야 새로고침없이 화면 갱신될거같음
-////   
-//   ModelAndView mv = new ModelAndView();
-//   
-//   
-//   //화면 뿌리기 //return mv 해야함 return null; }
-//   
-//   }
+   ModelAndView mv = new ModelAndView();
+   
+   
+   //화면 뿌리기 //return mv 해야함 return null; }
+   
+   }
