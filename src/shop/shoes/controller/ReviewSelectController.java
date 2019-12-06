3d118ed1a97f5d6cd9cf8bf330b2714b2c5dcad9
k312 <k31sw2@gmail.com>
2019-12-06 09:49:54 +0900

    검색창 수정2

--- src/shop/shoes/controller/ReviewSelectController.java
+++ src/shop/shoes/controller/ReviewSelectController.java
@@ -1,3 +1,4 @@
+/*
 
 package shop.shoes.controller;
 
@@ -13,20 +14,26 @@ import shop.shoes.service.ReviewService;
 
 public class ReviewSelectController implements Controller {
 
-	@Override public ModelAndView handleRequest(HttpServletRequest request,
-  HttpServletResponse response) throws SQLException, IOException {
+ @Override
+ public ModelAndView handleRequest(HttpServletRequest request,
+                                   HttpServletResponse response) throws SQLException, IOException {
   System.out.println("리뷰 select 컨트롤러 호출");
-  
-  
- try { List<ReviewDTO> list = ReviewService.selectAll(); //리뷰 list 받고
- request.setAttribute("reviewList", list); //set attribute 저장하는 name값 확인!!
- }catch(SQLException e) { e.printStackTrace(); }
- 
- ModelAndView mv = new ModelAndView(); // mv.setRedirect(isRedirect); 이동방식,
- 화면어디로 ? 정하기
- 
- 
- 
- //화면 뿌리기 //return mv 해야함 return null; }
- 
+
+
+  try {
+   List<ReviewDTO> list = ReviewService.selectAll(); //리뷰 list 받고
+   request.setAttribute("reviewList", list); //set attribute 저장하는 name값 확인!!
+  } catch (SQLException e) {
+   e.printStackTrace();
+  }
+
+  ModelAndView mv = new ModelAndView(); // mv.setRedirect(isRedirect); 이동방식,
+  */
+/* 화면어디로 ? 정하기*//*
+
+
+
+  //화면 뿌리기 //return mv 해야함 return null; }
+
  }
+}*/
